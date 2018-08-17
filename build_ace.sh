41fe59e7bc2c4d47075088f7b6e332bb39e20f6f
Edoardo Pasca <edo.paskino@gmail.com>
2018-08-17 13:54:29 +0000

    fix include install

--- build_ace.sh
+++ build_ace.sh
@@ -22,7 +22,7 @@
 version="6.4.7"
 threads=1
 is_build="1"
-while getopts p:v:j:s:i:b:l:h option
+while getopts p:v:j:s:l:i:b:h option
  do
  case "${option}"
   in
@@ -115,6 +115,6 @@ elif [ $target = "macosx" ] ; then
   cp -v ${ACE_ROOT}/ace/Compression/rle/libACE_RLECompression.dylib ${INSTALL_DIR}
   cp -v ${ACE_ROOT}/ace/Monitor_Control/libACE_Monitor_Control.dylib ${INSTALL_DIR}
 fi
-rsync -rv --include '*/' --include '*.h' --exclude '*' --prune-empty-dirs ${ACE_ROOT}/ace ${INCLUDE_DIR}/include
-rsync -rv --include '*/' --include '*.inl' --exclude '*' --prune-empty-dirs ${ACE_ROOT}/ace ${INCLUDE_DIR}/include
+rsync -rv --include '*/' --include '*.h' --exclude '*' --prune-empty-dirs ${ACE_ROOT}/ace ${INCLUDE_DIR}
+rsync -rv --include '*/' --include '*.inl' --exclude '*' --prune-empty-dirs ${ACE_ROOT}/ace ${INCLUDE_DIR}
 fi
