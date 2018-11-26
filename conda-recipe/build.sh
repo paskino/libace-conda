41f334bba78fa4a28b4abdc7529b0a04d5ab696d
Edoardo Pasca <edo.paskino@gmail.com>
2018-11-26 16:19:05 +0000

    remove ARCH env variable which confuses ACE build

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -2,7 +2,9 @@
 
 # in conda builds the BUILD environment variable is set and it annoys the 
 # build system of ACE
+export SAVE_BUILD=${BUILD}
 export BUILD=
+export ARCH=
 mkdir ${SRC_DIR}/build
 rsync -az --exclude=.git ${RECIPE_DIR}/../ ${SRC_DIR}/build
 mkdir ${SRC_DIR}/build/build
@@ -14,6 +16,20 @@ cmake -G "Unix Makefiles" \
   -DINCLUDE_DIR="${PREFIX}/include" \
   --debug-output ../
 
+#cmake -G "Unix Makefiles" \
+#  -DCMAKE_BUILD_TYPE=Release \
+#  -DLIBRARY_LIB="${CONDA_PREFIX}/lib" \
+#  -DLIBRARY_INC="${CONDA_PREFIX}"\
+#  -DLIBRARY_DIR="${CONDA_PREFIX}/lib" \
+#  -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+#  -DINSTALL_LIB_DIR="${PREFIX}/lib" \
+#  -DINSTALL_BIN_DIR="${PREFIX}/bin" \
+#  -DINSTALL_INCLUDE_DIR="${PREFIX}/include" \
+#  -DCMAKE_INSTALL_PREFIX="${PREFIX}"\
+#  --debug-output ../
+
+
 #cmake -G "Unix Makefiles" -DLIBRARY_LIB="%CONDA_PREFIX%\lib" -DLIBRARY_INC="%CONDA_PREFIX%" -DCMAKE_INSTALL_PREFIX="%PREFIX%\Library" -DINSTALL_LIB_DIR="%PREFIX%\Library\lib" -DINSTALL_BIN_DIR="%PREFIX%\Library\bin" -DINSTALL_INCLUDE_DIR="%PREFIX%\Library\include" "%SRC_DIR%\build" 
 
-make -j1 VERBOSE=1
+make -j2 VERBOSE=1
+export BUILD=${SAVE_BUILD}
